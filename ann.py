1940ffc7184473ee36e31796186d698199cf2a16
Brandon Edmunds <brandonedmunds2@gmail.com>
2023-02-27 15:14:43 -0500

    add preprocessing. read readme

---
+++ ann.py
@@ -0,0 +1,110 @@
+import utils
+import numpy as np
+import torch
+import random
+import torch.nn.functional as F
+from torch import nn, optim
+from torch.utils.data import TensorDataset
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+np.random.seed(0)
+random.seed(0)
+torch.manual_seed(0)
+
+LR=0.01
+EPOCHS=10
+TRAIN_BATCH=16
+TEST_BATCH=1024
+
+NUM_WORKERS=0
+
+use_cuda = torch.cuda.is_available()
+device = torch.device('cuda' if use_cuda else 'cpu')
+
+class ANN(nn.Module):
+    def __init__(self,in_features,num_classes):
+        super().__init__()
+        self.fc1=nn.Linear(in_features=in_features,out_features=512)
+        self.fc2=nn.Linear(in_features=512,out_features=num_classes)
+    def forward(self,x):
+        pred=F.relu(self.fc1(x))
+        pred=self.fc2(pred)
+        return pred
+    
+def train(train_loader, model, optimizer, criterion):
+    model=model.train()
+    losses = []
+    correct=0
+    incorrect=0
+    for data, target in train_loader:
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+        losses.append(loss.item())
+        correct += torch.sum(output.argmax(axis=1) == target.argmax(axis=1))
+        incorrect += torch.sum(output.argmax(axis=1) != target.argmax(axis=1))
+    return np.mean(losses), (100.0 * correct / (correct+incorrect))
+
+def test(test_loader, model, criterion):
+    model=model.eval()
+    losses = []
+    correct = 0
+    incorrect=0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            losses.append(criterion(output, target).item())
+            correct += torch.sum(output.argmax(axis=1) == target.argmax(axis=1))
+            incorrect += torch.sum(output.argmax(axis=1) != target.argmax(axis=1))
+    return np.mean(losses), (100.0 * correct / (correct+incorrect))
+
+def load_data():
+    features,labels=utils.preproc_data(*utils.load_data())
+    x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=4)
+    train_loader=torch.utils.data.DataLoader(
+        TensorDataset(torch.from_numpy(x_train),torch.from_numpy(y_train)),
+        batch_size=TRAIN_BATCH,
+        num_workers=NUM_WORKERS,
+        pin_memory=True
+    )
+    test_loader=torch.utils.data.DataLoader(
+        TensorDataset(torch.from_numpy(x_test),torch.from_numpy(y_test)),
+        batch_size=TEST_BATCH,
+        num_workers=NUM_WORKERS,
+        pin_memory=True
+    )
+    return train_loader, test_loader, features.shape[1], labels.shape[1]
+
+def plt_losses(train_losses,test_losses):
+    plt.figure()
+    plt.plot(range(EPOCHS),train_losses, label="Train Loss")
+    plt.plot(range(EPOCHS),test_losses, label="Test Loss")
+    plt.title('Train and Test Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend(loc='best')
+    plt.show()
+
+def main():
+    train_loader,test_loader,in_features,num_classes=load_data()
+    model=ANN(in_features=in_features,num_classes=num_classes)
+    model=model.to(device)
+    criterion=nn.CrossEntropyLoss()
+    optimizer=optim.SGD(model.parameters(),lr=LR)
+    train_losses=[]
+    test_losses=[]
+    for epoch in range(EPOCHS):
+        train_loss,train_acc=train(train_loader,model,optimizer,criterion)
+        train_losses.append(train_loss)
+        test_loss,test_acc=test(test_loader,model,criterion)
+        test_losses.append(test_loss)
+        print(f'Epoch: {epoch}, Train Loss: {train_loss}, Test Loss: {test_loss}')
+    plt_losses(train_losses,test_losses)
+    print(f'Accuracy: {test_acc}')
+
+main()
---
+++ utils.py
@@ -0,0 +1,105 @@
+import pandas as pd
+import numpy as np
+import yfinance as yf
+import os
+
+CODE2STOCK={"UA":"UAL","AA":"AAL","AS":"ALK","B6":"JBLU","DL":"DAL","HA":"HA","NK":"SAVE","OO":"SKYW","WN":"LUV","G4":"ALGT"}
+NO_STOCK=["EV","VX","9E","MQ","OH","YX","QX","F9","YV"]
+
+def _clean_bts_data(input_dir_path="./raw_bts_data",output_path="./data/bts_data.csv"):
+    df=None
+    for f in os.listdir(input_dir_path):
+        p = os.path.join(input_dir_path, f)
+        if(type(df)==None):
+            df=pd.read_csv(p)
+        else:
+            df=pd.concat([df,pd.read_csv(p)])
+    df=df.dropna()
+    for s in NO_STOCK:
+        df=df[df["OP_UNIQUE_CARRIER"]!=s]
+    df.to_csv(output_path,index=False)    
+
+def _get_dow(input_path="./raw_stock_data/dow_data.csv"):
+    df=pd.read_csv(input_path)
+    df["Stock"]="DOW"
+    df["YEAR"]=df.apply(lambda row: int(row["Date"].split("/")[2]),axis=1)
+    df["MONTH"]=df.apply(lambda row: int(row["Date"].split("/")[0]),axis=1)
+    df["DAY_OF_MONTH"]=df.apply(lambda row: int(row["Date"].split("/")[1]),axis=1)
+    df=df.drop("Date",axis=1)
+    return df
+
+def _clean_stock_data(input_dow_path="./raw_stock_data/dow_data.csv",output_path="./data/stock_data.csv"):
+    ranges=[("2017-12-01","2017-12-31"),
+            ("2018-12-01","2018-12-31"),
+            ("2019-12-01","2019-12-31"),
+            ("2020-12-01","2020-12-31"),
+            ("2021-12-01","2021-12-31"),
+            ]
+    stocks=list(CODE2STOCK.values())
+    stocks.append("^IXIC")
+    data=None
+    for stock in stocks:
+        for range in ranges:
+            new = yf.download(stock, start=range[0], end=range[1])
+            new["Stock"]=stock
+            if(type(data)==None):
+                data=new
+            else:
+                data=pd.concat([data,new])
+    data=data.reset_index()
+    data=data[["Date","Close","Stock"]]
+    data["YEAR"]=data.apply(lambda row: row["Date"].year,axis=1)
+    data["MONTH"]=data.apply(lambda row: row["Date"].month,axis=1)
+    data["DAY_OF_MONTH"]=data.apply(lambda row: row["Date"].day,axis=1)
+    data=data.drop("Date",axis=1)
+    dow=_get_dow(input_dow_path)
+    df=pd.concat([data,dow])
+    df.to_csv(output_path,index=False)
+
+def _match_stock(row,stock,stock_name):
+    vals=stock[(stock["YEAR"]==row["YEAR"]) & (stock["MONTH"]==row["MONTH"]) & (stock["DAY_OF_MONTH"]==row["DAY_OF_MONTH"]) & (stock["Stock"]==stock_name)]["Close"].values
+    if(len(vals)==0):
+        return pd.NA
+    else:
+        return vals[0]
+
+def _merge_bts_stock(bts_path="./data/bts_data.csv",stock_path="./data/stock_data.csv",output_path="./data/all_data.csv"):
+    bts=pd.read_csv(bts_path)
+    stock=pd.read_csv(stock_path)
+    bts["NASDAQ"]=bts.apply(lambda row: _match_stock(row,stock,"^IXIC"),axis=1)
+    bts["DOW"]=bts.apply(lambda row: _match_stock(row,stock,"DOW"),axis=1)
+    bts["AIR_STOCK"]=bts.apply(lambda row: _match_stock(row,stock,CODE2STOCK[row["OP_UNIQUE_CARRIER"]]),axis=1)
+    df=bts.dropna()
+    df.to_csv(output_path,index=False)
+
+def load_data(input_path="./data/all_data.csv"):
+    df=pd.read_csv(input_path)
+    labels=df["ARR_DEL15"].to_frame()
+    features=df.drop("ARR_DEL15",axis=1)
+    return features,labels
+
+def _preproc_features(df,max_uniques=60):
+    no_1hot_cols=["AIR_STOCK","DOW","NASDAQ","DEP_TIME","CRS_DEP_TIME","DEP_DELAY","DEP_DELAY_NEW","DEP_DEL15","TAXI_OUT","WHEELS_OFF","WHEELS_ON","TAXI_IN","CRS_ARR_TIME","ARR_TIME","ARR_DELAY","ARR_DELAY_NEW","ARR_DEL15","CANCELLED","DIVERTED","CRS_ELAPSED_TIME","ACTUAL_ELAPSED_TIME","AIR_TIME","FLIGHTS","DISTANCE"]
+    for col in df.columns:
+        if(len(df[col].unique())<=1):
+            df=df.drop(col,axis=1)
+    cols=df.columns
+    cols=[col for col in cols if col not in no_1hot_cols]
+    for col in cols:
+        if(len(df[col].unique())>=max_uniques):
+            df=df.drop(col,axis=1)
+        else:
+            one_hot = pd.get_dummies(df[col],prefix=col)
+            df = df.drop(col,axis = 1)
+            df = df.join(one_hot)
+    df=(df-df.mean())/df.std()
+    return df.to_numpy(dtype=np.float32)
+
+def _preproc_labels(df):
+    one_hot = pd.get_dummies(df["ARR_DEL15"],prefix="ARR_DEL15")
+    df = df.drop("ARR_DEL15",axis = 1)
+    df = df.join(one_hot)
+    return df.to_numpy(dtype=np.float32)
+
+def preproc_data(features, labels):
+    return _preproc_features(features),_preproc_labels(labels)
